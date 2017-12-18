acb83b51e664bb0346c749c7d4982014ec1e6230
Hoo Yu Heng <hooyuheng@gmail.com>
2017-12-18 18:12:54 +0800

    fix repeated variables with global, added the window

--- keras_basic_2.py
+++ keras_basic_2.py
@@ -6,14 +6,13 @@ import numpy as np
 np.random.seed(7)
 
 # initialize training data set
-raw_data, data_x, data_y, data_x_processed, data_y_processed, char_to_int, int_to_char = \
-    kb1.state_within_a_batch_data(1)  # the sequence length
-
+data_x, data_y, data_x_processed, data_y_processed = \
+    kb1.n_char_to_one_char_data(sequence_length=3, window='feature')  # the sequence length
 
 # instantiate the lstm network
-lstm_network = kb1.LstmNetwork(raw_data, data_x, data_y, data_x_processed, data_y_processed, char_to_int, int_to_char)
+lstm_network = kb1.LstmNetwork(data_x, data_y, data_x_processed, data_y_processed)
 # training
-lstm_network.training(nb_epochs=5000, batch_size=len(data_x), shuffle=False)
+lstm_network.training(nb_epochs=500, batch_size=1, shuffle=True)
 # test accuracy
 lstm_network.test_accuracy()
 # prediction visualization
@@ -23,7 +22,7 @@ lstm_network.predict_random()
 
 
 # Data Visualization before Training, Uncomment to watch if u are confused
-print('raw data:\n', raw_data)
+print('raw data:\n', kb1.alphabet)
 print('Data_x:\n', data_x)
 print('Data_y:\n', data_y)
 print('Data_x_processed:\n', data_x_processed)
--- keras_basics_1.py
+++ keras_basics_1.py
@@ -5,24 +5,27 @@ from keras.layers import LSTM
 from keras.utils import np_utils
 from keras.preprocessing.sequence import pad_sequences
 
+# global variables
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+char_to_int = dict((c, i) for i, c in enumerate(alphabet))
+int_to_char = dict((i, c) for i, c in enumerate(alphabet))
+
+
 class LstmNetwork:
     # declaring class global fields
     model = Sequential()
+    # accessing the global variables
+    global alphabet, char_to_int, int_to_char
 
     # raw_data is the series of alphabets 'ABC...Z'
     # data_x, data_y are list of unscaled inputs and labels
     # data_x_processed and data_y_processed are those processed, ready for supervised training
     # char_to_int and int_to_char are both just dict {'A':0, ..} and {0:'A', ...}
-    def __init__(self, raw_data, data_x, data_y, data_x_processed, data_y_processed, char_to_int, int_to_char):
-        self.alphabet = raw_data
+    def __init__(self, data_x, data_y, data_x_processed, data_y_processed):
         self.x = data_x
         self.y = data_y
         self.inputs = data_x_processed
         self.labels = data_y_processed
-        # create a dict of mappings btw every single char in alphabet to index 0-*
-        # e.g. {'A':0, 'B':1, ...}
-        self.char_map_int = char_to_int
-        self.int_map_char = int_to_char
 
     # inputs and labels comes in pairs, as a supervised training set
     # nb_epochs is no of training it carries out with the same data set
@@ -66,12 +69,12 @@ class LstmNetwork:
 
     def predict_all(self):
         for unit_input in self.x:
-            x = np.reshape(unit_input, (1, len(unit_input), 1))
-            x = x / float(len(self.alphabet))
+            x = np.reshape(unit_input, (1, self.inputs.shape[1], self.inputs.shape[2]))
+            x = x / float(len(alphabet))
             prediction = self.model.predict(x, verbose=0)
             index = np.argmax(prediction)
-            result = self.int_map_char[index]  # yh: index might not b int
-            seq_in = [self.int_map_char[values] for values in unit_input]
+            result = int_to_char[index]  # yh: index might not b int
+            seq_in = [int_to_char[values] for values in unit_input]
             print(seq_in, '->', result)
 
     def predict_random(self):
@@ -80,17 +83,17 @@ class LstmNetwork:
             unit_input_index = np.random.randint(len(self.x))
             unit_input = self.x[unit_input_index]
             # convert into [samples, time steps, features]
-            x = np.reshape(unit_input, (1, len(unit_input), 1))
-            x = x / float(len(self.alphabet))
+            x = np.reshape(unit_input, (1, self.inputs.shape[1], self.inputs.shape[2]))
+            x = x / float(len(alphabet))
             prediction = self.model.predict(x, verbose=0)
             index = np.argmax(prediction)
-            result = self.int_map_char[index]
-            seq_in = [self.int_map_char[values] for values in unit_input]
+            result = int_to_char[index]
+            seq_in = [int_to_char[values] for values in unit_input]
             print(seq_in, '->', result)
 
 
-# this creates a data set for One-Char to One-Char Mapping by Stateful LSTM
-def one_char_to_one_char_data(sequence_length):
+# this creates a data set for n-Char to One-Char Mapping by LSTM
+def n_char_to_one_char_data(sequence_length, window):
     # define sequence length, e.g. for =2: 'A','B' for 'C'; for =3: 'A','B','C' for 'D'
     # this sequence is to supervised training the network
     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -98,19 +101,22 @@ def one_char_to_one_char_data(sequence_length):
     data_x, data_y = [], []
     # create a dict of mappings btw every single char in alphabet to index 0-*
     # e.g. {'A':0, 'B':1, ...}
-    char_map_int = dict((c, i) for i, c in enumerate(alphabet))
-    int_map_char = dict((i, c) for i, c in enumerate(alphabet))
-    # mapping of 'A' to 0, ....
+    # create a supervised training set
     for i in range(0, len(alphabet) - seq_length, 1):
-        # extract section of the alphabet and store
+        # initialize seq_in = 'A', seq_out = 'B' and so on for seq_len=1
+        # initialize seq_in = 'ABC', seq_out = 'D' and so on for seq_len=3
         seq_in = alphabet[i: i + seq_length]
         seq_out = alphabet[i + seq_length]
-        # save another copy of int rep of the alphabets
-        # [] is used to contain all seq in one single index of the array data_x
-        data_x.append([char_map_int[char] for char in seq_in])
-        data_y.append([char_map_int[char] for char in seq_out])
+        # next convert the char(in seq_in & seq_out) to int, e.g.
+        # data_x = [[0], [1], ...], data_y = [[1], [2], ...] for seq_len=1
+        # data_x = [[0, 1, 2], [2, 3, 4], ...], data_y = [[3], [4], ...] for seq_len=3
+        data_x.append([char_to_int[char] for char in seq_in])
+        data_y.append([char_to_int[char] for char in seq_out])
     # reshape X to be [samples, time steps, features]
-    data_x_processed = np.reshape(data_x, (len(data_x), seq_length, 1))
+    if window == 'time_step':
+        data_x_processed = np.reshape(data_x, (len(data_x), seq_length, 1))
+    elif window == 'feature':
+        data_x_processed = np.reshape(data_x, (len(data_x), 1, seq_length))
     # normalize
     data_x_processed = data_x_processed / float(len(alphabet))
     # one hot encode the output variable, meaning convert 3 to 00010...0, 4 to 000010...0, and so on
@@ -118,7 +124,7 @@ def one_char_to_one_char_data(sequence_length):
     # neuron at output layer
     data_y_processed = np_utils.to_categorical(data_y)
 
-    return alphabet, data_x, data_y, data_x_processed, data_y_processed, char_map_int, int_map_char
+    return data_x, data_y, data_x_processed, data_y_processed
 
 
 def state_within_a_batch_data(sequence_length):
@@ -149,5 +155,4 @@ def state_within_a_batch_data(sequence_length):
     data_y_processed = np_utils.to_categorical(data_y)
 
     return alphabet, data_x, data_y, data_x_processed, data_y_processed, char_map_int, int_map_char
-
 
