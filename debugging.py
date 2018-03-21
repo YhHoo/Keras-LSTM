ef3c9b3da93bfbbb7435cb63313f1de28b5ad592
Hoo Yu Heng <hooyuheng@gmail.com>
2018-03-21 19:22:16 +0800

    error on line333

--- debugging.py
+++ debugging.py
@@ -1,10 +1,14 @@
 import numpy as np
+from math import sin, radians
+import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
 from keras.optimizers import SGD
-from sklearn.preprocessing import MinMaxScaler
-
-# 1 sample, 10 time steps, and 1 feature
+from sklearn.preprocessing import StandardScaler
+data = np.array([1, 2, 3, 4, 5]).reshape((5, 1))
+scaler = StandardScaler()
+data_scaled = scaler.fit_transform(data)
+print(data_scaled)
 
 
 def difference(datalist, interval=1):
@@ -21,11 +25,10 @@ def inverse_difference(diff_list, datalist):
     return inv_list
 
 
-data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-data2 = difference(data)
-print(data2)
-data3 = inverse_difference(data2, data)
-print(data3)
+# sine_wave = [sin(radians(x)) for x in range(360)]
+# sine_wave = sine_wave * 2
+# plt.plot(sine_wave)
+# plt.show()
 
 # data2 = np.array([[11, 22, 33, 44, 55, 66, 77, 88, 99, 110]])
 # data3 = np.concatenate((data, data2), axis=0)
@@ -48,8 +51,8 @@ print(data3)
 
 # scaler = MinMaxScaler(feature_range=(0, 1))
 #
-dummy = np.array([[1, 2, 3, 4, 5],
-                  [11, 22, 33, 44, 55],
-                  [9, 9, 9, 9, 99],
-                  [5, 5, 5, 5, 500]])
+# dummy = np.array([[1, 2, 3, 4, 5],
+#                   [11, 22, 33, 44, 55],
+#                   [9, 9, 9, 9, 99],
+#                   [5, 5, 5, 5, 500]])
 # dummy[1:3, 2:4] = np.array([[7, 7], [7, 7]])
